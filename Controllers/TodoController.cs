7f4c4237322e76e18672f056b55fce807f6c808b
TRAN NAM HAI <haitn@mytrax.co.jp>
2017-11-03 17:29:15 +0700

    MVCbasics

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Services;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            var todoItems = await _todoItemService.GetIncompleteItemsAsync();
+            return View();
+        }
+    }
+}
