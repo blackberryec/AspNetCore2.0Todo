63c4ed8f3cc268f6fae242f064b3d41b5bb510e8
HAITRAN\trann <trannamhaibp@gmail.com>
2017-11-04 23:57:32 +0700

    finished add more features

--- Services/TodoItemService.cs
+++ Services/TodoItemService.cs
@@ -16,11 +16,42 @@ namespace AspNetCoreTodo.Services
         {
             _context = context;
         }
+
+        public async Task<bool> AddItemAsync(NewTodoItem newItem)
+        {
+            var entity = new TodoItem
+            {
+                Id = Guid.NewGuid(),
+                IsDone = false,
+                Title = newItem.Title,
+                DueAt = DateTimeOffset.Now.AddDays(3)
+            };
+
+            _context.Items.Add(entity);
+
+            var saveResult = await _context.SaveChangesAsync();
+            return saveResult == 1;
+        }
+
         public async Task<IEnumerable<TodoItem>> GetIncompleteItemsAsync()
         {
             return await _context.Items
                  .Where(x => x.IsDone == false)
                  .ToArrayAsync();
+        }
+
+        public async Task<bool> MarkDoneAsync(Guid id)
+        {
+            var item = await _context.Items
+                .Where(x => x.Id == id)
+                .SingleOrDefaultAsync();
+
+            if (item == null) return false;
+
+            item.IsDone = true;
+
+            var saveResult = await _context.SaveChangesAsync();
+            return saveResult == 1; //one entity should have been updated
         }
     }
 }
