043a6f6fea16068374492496f2e93931802d6d6e
HAITRAN\trann <trannamhaibp@gmail.com>
2017-11-04 21:52:22 +0700

    Use a database by EF

---
+++ Services/TodoItemService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreTodo.Services
+{
+    public class TodoItemService : ITodoItemService
+    {
+        public readonly ApplicationDbContext _context;
+
+        public TodoItemService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<TodoItem>> GetIncompleteItemsAsync()
+        {
+            return await _context.Items
+                 .Where(x => x.IsDone == false)
+                 .ToArrayAsync();
+        }
+    }
+}
